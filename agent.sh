2772e79a8db52c1e40a2f81642347eafac7d7605
trazoss <97839358+trazoss@users.noreply.github.com>
2023-03-08 18:44:17 +1000

    Create agent.sh

---
+++ agent.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+MAIN() {
+  live=$(ps -AL | grep tinyproxy | wc -l)
+  ip=$(hostname -I)
+  host="$(hostname -f)"
+  data='''{"KEY":"SEC_KEY100200300","host":"'$host'","ip":"'$ip'","live":"'$live'","rtx":'"$(vnstat -tr --json)"'}'''
+  curl -X POST \
+    http://marvelhost.co/monitor/agent/ \
+    -H 'Accept: application/json' \
+    -H 'Content-Type: application/json; charset=UTF-8' \
+    --data "$data"
+}
+
+MAIN
